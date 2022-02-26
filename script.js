f129b746a622b12bc67bf52be35288e03b736ca7
millerm30 <millerm30@icloud.com>
2022-02-25 22:00:33 -0330

    Initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// Variables
+
+let currentInput = document.querySelector(".currentInput");
+let answerScreen = document.querySelector(".answerScreen");
+let buttons = document.querySelectorAll("button");
+let erasebtn = document.querySelector("#erase");
+let clearbtn = document.querySelector("#clear");
+let evaluate = document.querySelector("#evaluate");
+
+// Calculator Display
+
+let realTimeScreenValue = [];
+
+// To Clear
+
+clearbtn.addEventListener("click", () => {
+  realTimeScreenValue = [""];
+  answerScreen.innerHTML = 0;
+  currentInput.className = "currentInput";
+  answerScreen.className = "answerScreen";
+  answerScreen.style.color = " rgba(150, 150, 150, 0.87)";
+});
+
+// Get value of any button clicked and display to the screen
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // when clicked button is not clear button
+    if (!btn.id.match("erase")) {
+      // To display value on btn press
+      realTimeScreenValue.push(btn.value);
+      currentInput.innerHTML = realTimeScreenValue.join("");
+
+      // To evaluate answer in real time
+      if (btn.classList.contains("num_btn")) {
+        answerScreen.innerHTML = eval(realTimeScreenValue.join(""));
+      }
+    }
+
+    // When clicked button is evaluate button
+    if (btn.id.match("evaluate")) {
+      currentInput.className = "answerScreen";
+      answerScreen.className = "currentInput";
+      answerScreen.style.color = "white";
+    }
+  });
+});
